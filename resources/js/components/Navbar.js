77546bfff4e61f325b55a020bbbc685fec1a9225
sanyath007 <sanyath007@gmail.com>
2022-12-09 21:36:41 +0700

    Modify Navbar component by create new handleToggleSidebarBtn method for toggling sidebar and use process.env to set app's url of all image src

--- resources/js/components/Navbar.js
+++ resources/js/components/Navbar.js
@@ -7,17 +7,25 @@ const Navbar = ({ menuCollapsed, handleMenuCollapsed }) => {
     const navigate = useNavigate()
 
     const handleLogout = (e) => {
+        e.preventDefault()
+
         return navigate('/login')
+    }
+
+    const handleToggleSidebarBtn = (e) => {
+        const body = document.querySelector('body')
+
+        body.classList.toggle('toggle-sidebar')
     }
 
     return (
         <header id="header" className="header fixed-top d-flex align-items-center">
             <div className="d-flex align-items-center justify-content-between">
                 <a href="#" className="logo d-flex align-items-center">
-                    <img src="http://localhost:8000/laravel7-webapi-demo01/public/img/logo.png" alt="" />
-                    <span className="d-none d-lg-block">Admin</span>
+                    <img src={`${process.env.MIX_APP_URL}/img/logo.png`} alt="" />
+                    <span className="d-none d-lg-block">OccMed</span>
                 </a>
-                <i className="bi bi-list toggle-sidebar-btn"></i>
+                <i className="bi bi-list toggle-sidebar-btn" onClick={handleToggleSidebarBtn}></i>
             </div>
             <div className="search-bar">
                 <form className="search-form d-flex align-items-center" method="POST" action="#">
@@ -27,156 +35,174 @@ const Navbar = ({ menuCollapsed, handleMenuCollapsed }) => {
             </div>
             <nav className="header-nav ms-auto">
                 <ul className="d-flex align-items-center">
-                <li className="nav-item d-block d-lg-none">
-                    <a className="nav-link nav-icon search-bar-toggle" href="#">
-                        <i className="bi bi-search"></i>
-                    </a>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link nav-icon" href="#" data-bs-toggle="dropdown"> <i className="bi bi-bell"></i> <span className="badge bg-primary badge-number">4</span> </a>
-                    <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
-                        <li className="dropdown-header">
-                            You have 4 new notifications
-                            <a href="#"><span className="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
-                        </li>
-                        <li>
-                            <hr className="dropdown-divider" />
-                        </li>
-                        <li className="notification-item">
-                            <i className="bi bi-exclamation-circle text-warning"></i>
-                            <div>
-                            <h4>Lorem Ipsum</h4>
-                            <p>Quae dolorem earum veritatis oditseno</p>
-                            <p>30 min. ago</p>
-                            </div>
-                        </li>
-                        <li>
-                            <hr className="dropdown-divider" />
-                        </li>
-                        <li className="notification-item">
-                            <i className="bi bi-x-circle text-danger"></i>
-                            <div>
-                            <h4>Atque rerum nesciunt</h4>
-                            <p>Quae dolorem earum veritatis oditseno</p>
-                            <p>1 hr. ago</p>
-                            </div>
-                        </li>
-                        <li>
-                            <hr className="dropdown-divider" />
-                        </li>
-                        <li className="notification-item">
-                            <i className="bi bi-check-circle text-success"></i>
-                            <div>
-                            <h4>Sit rerum fuga</h4>
-                            <p>Quae dolorem earum veritatis oditseno</p>
-                            <p>2 hrs. ago</p>
-                            </div>
-                        </li>
-                        <li>
-                            <hr className="dropdown-divider" />
-                        </li>
-                        <li className="notification-item">
-                            <i className="bi bi-info-circle text-primary"></i>
-                            <div>
-                            <h4>Dicta reprehenderit</h4>
-                            <p>Quae dolorem earum veritatis oditseno</p>
-                            <p>4 hrs. ago</p>
-                            </div>
-                        </li>
-                        <li>
-                            <hr className="dropdown-divider" />
-                        </li>
-                        <li className="dropdown-footer"> <a href="#">Show all notifications</a></li>
-                    </ul>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-                        <i className="bi bi-chat-left-text"></i>
-                        <span className="badge bg-success badge-number">3</span>
-                    </a>
-                    <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
-                        <li className="dropdown-header">
-                            You have 3 new messages
-                            <a href="#"><span className="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
-                        </li>
-                        <li>
-                            <hr className="dropdown-divider" />
-                        </li>
-                        <li className="message-item">
-                            <a href="#">
-                            <img src="http://localhost:8000/laravel7-webapi-demo01/public/img/messages-1.jpg" alt="" className="rounded-circle" />
-                            <div>
-                                <h4>Jassa</h4>
-                                <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                                <p>4 hrs. ago</p>
-                            </div>
-                            </a>
-                        </li>
-                        <li>
-                            <hr className="dropdown-divider" />
-                        </li>
-                        <li className="message-item">
-                            <a href="#">
-                            <img src="http://localhost:8000/laravel7-webapi-demo01/public/img/messages-2.jpg" alt="" className="rounded-circle" />
-                            <div>
-                                <h4>Jassa</h4>
-                                <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                                <p>6 hrs. ago</p>
-                            </div>
-                            </a>
-                        </li>
-                        <li>
-                            <hr className="dropdown-divider" />
-                        </li>
-                        <li className="message-item">
-                            <a href="#">
-                            <img src="http://localhost:8000/laravel7-webapi-demo01/public/img/messages-3.jpg" alt="" className="rounded-circle" />
-                            <div>
-                                <h4>Jassa</h4>
-                                <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                                <p>8 hrs. ago</p>
-                            </div>
-                            </a>
-                        </li>
-                        <li>
-                            <hr className="dropdown-divider" />
-                        </li>
-                        <li className="dropdown-footer"> <a href="#">Show all messages</a></li>
-                    </ul>
-                </li>
-                <li className="nav-item dropdown pe-3">
-                    <a className="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                        <img src="http://localhost:8000/laravel7-webapi-demo01/public/img/profile-img.jpg" alt="Profile" className="rounded-circle" />
-                        <span className="d-none d-md-block dropdown-toggle ps-2">Jassa</span>
-                    </a>
-                    <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-                        <li className="dropdown-header">
-                            <h6>Jassa</h6>
-                            <span>Web Designer</span>
-                        </li>
-                        <li>
-                            <hr className="dropdown-divider" />
-                        </li>
-                        <li>
-                            <a className="dropdown-item d-flex align-items-center" href="users-profile.html">
-                                <i className="bi bi-person"></i>
-                                <span>My Profile</span>
-                            </a>
-                        </li>
-                        <li>
-                            <hr className="dropdown-divider" />
-                        </li>
-                        <li> <a className="dropdown-item d-flex align-items-center" href="users-profile.html"> <i className="bi bi-gear"></i> <span>Account Settings</span> </a></li>
-                        <li>
-                            <hr className="dropdown-divider" />
-                        </li>
-                        <li> <a className="dropdown-item d-flex align-items-center" href="pages-faq.html"> <i className="bi bi-question-circle"></i> <span>Need Help?</span> </a></li>
-                        <li>
-                            <hr className="dropdown-divider" />
-                        </li>
-                        <li> <a className="dropdown-item d-flex align-items-center" href="#"> <i className="bi bi-box-arrow-right"></i> <span>Sign Out</span> </a></li>
-                    </ul>
-                </li>
+                    <li className="nav-item d-block d-lg-none">
+                        <a className="nav-link nav-icon search-bar-toggle" href="#">
+                            <i className="bi bi-search"></i>
+                        </a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+                            <i className="bi bi-bell"></i>
+                            <span className="badge bg-primary badge-number">4</span>
+                        </a>
+                        <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
+                            <li className="dropdown-header">
+                                You have 4 new notifications
+                                <a href="#"><span className="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+                            </li>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <li className="notification-item">
+                                <i className="bi bi-exclamation-circle text-warning"></i>
+                                <div>
+                                    <h4>Lorem Ipsum</h4>
+                                    <p>Quae dolorem earum veritatis oditseno</p>
+                                    <p>30 min. ago</p>
+                                </div>
+                            </li>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <li className="notification-item">
+                                <i className="bi bi-x-circle text-danger"></i>
+                                <div>
+                                    <h4>Atque rerum nesciunt</h4>
+                                    <p>Quae dolorem earum veritatis oditseno</p>
+                                    <p>1 hr. ago</p>
+                                </div>
+                            </li>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <li className="notification-item">
+                                <i className="bi bi-check-circle text-success"></i>
+                                <div>
+                                    <h4>Sit rerum fuga</h4>
+                                    <p>Quae dolorem earum veritatis oditseno</p>
+                                    <p>2 hrs. ago</p>
+                                </div>
+                            </li>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <li className="notification-item">
+                                <i className="bi bi-info-circle text-primary"></i>
+                                <div>
+                                    <h4>Dicta reprehenderit</h4>
+                                    <p>Quae dolorem earum veritatis oditseno</p>
+                                    <p>4 hrs. ago</p>
+                                </div>
+                            </li>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <li className="dropdown-footer"> <a href="#">Show all notifications</a></li>
+                        </ul>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+                            <i className="bi bi-chat-left-text"></i>
+                            <span className="badge bg-success badge-number">3</span>
+                        </a>
+                        <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
+                            <li className="dropdown-header">
+                                You have 3 new messages
+                                <a href="#"><span className="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+                            </li>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <li className="message-item">
+                                <a href="#">
+                                <img src={`${process.env.MIX_APP_URL}/img/messages-1.jpg`} alt="" className="rounded-circle" />
+                                <div>
+                                    <h4>Jassa</h4>
+                                    <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
+                                    <p>4 hrs. ago</p>
+                                </div>
+                                </a>
+                            </li>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <li className="message-item">
+                                <a href="#">
+                                <img src={`${process.env.MIX_APP_URL}/img/messages-2.jpg`} alt="" className="rounded-circle" />
+                                <div>
+                                    <h4>Jassa</h4>
+                                    <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
+                                    <p>6 hrs. ago</p>
+                                </div>
+                                </a>
+                            </li>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <li className="message-item">
+                                <a href="#">
+                                <img src={`${process.env.MIX_APP_URL}/img/messages-3.jpg`} alt="" className="rounded-circle" />
+                                <div>
+                                    <h4>Jassa</h4>
+                                    <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
+                                    <p>8 hrs. ago</p>
+                                </div>
+                                </a>
+                            </li>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <li className="dropdown-footer"> <a href="#">Show all messages</a></li>
+                        </ul>
+                    </li>
+                    <li className="nav-item dropdown pe-3">
+                        <a className="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+                            <img src={`${process.env.MIX_APP_URL}/img/profile-img.jpg`} alt="Profile" className="rounded-circle" />
+                            <span className="d-none d-md-block dropdown-toggle ps-2">Jassa</span>
+                        </a>
+                        <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+                            <li className="dropdown-header">
+                                <h6>Jassa</h6>
+                                <span>Web Designer</span>
+                            </li>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <li>
+                                <a className="dropdown-item d-flex align-items-center" href="users-profile.html">
+                                    <i className="bi bi-person"></i>
+                                    <span>My Profile</span>
+                                </a>
+                            </li>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <li>
+                                <a className="dropdown-item d-flex align-items-center" href="users-profile.html">
+                                    <i className="bi bi-gear"></i>
+                                    <span>Account Settings</span>
+                                </a>
+                            </li>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <li>
+                                <a className="dropdown-item d-flex align-items-center" href="pages-faq.html">
+                                    <i className="bi bi-question-circle"></i>
+                                    <span>Need Help?</span>
+                                </a>
+                            </li>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <li>
+                                <a className="dropdown-item d-flex align-items-center" href="#" onClick={handleLogout}>
+                                    <i className="bi bi-box-arrow-right"></i>
+                                    <span>Sign Out</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
                 </ul>
             </nav>
         </header>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
 
         <!-- csrf token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Your Website Title</title>
+        <title>MNRH Occ Med</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
