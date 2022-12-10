8df01d15833b60c0622df49ea8cc13a349ebb76f
sanyath007 <sanyath007@gmail.com>
2022-12-10 21:40:36 +0700

    Re-design pages/Login component

--- resources/js/pages/Login.js
+++ resources/js/pages/Login.js
@@ -9,21 +9,64 @@ const Login = () => {
     }
 
     return (
-        <div className="login-page">
-            <div className="avatar">
-                <img src="https://banner2.cleanpng.com/20180505/upw/kisspng-computer-icons-avatar-businessperson-interior-desi-corporae-5aee195c6d1683.4671087315255535004468.jpg" alt="" />
+        <main>
+            <div className="container">
+                <section className="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+                                <div className="d-flex justify-content-center py-4">
+                                    <a href="index.html" className="logo d-flex align-items-center w-auto">
+                                        <img src={`${process.env.MIX_APP_URL}/img/logo.png`} alt="" />
+                                        <span className="d-none d-lg-block">Admin</span>
+                                    </a>
+                                </div>
+                                <div className="card mb-3">
+                                <div className="card-body">
+                                    <div className="pt-4 pb-2">
+                                        <h5 className="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                                        <p className="text-center small">Enter your username & password to login</p>
+                                    </div>
+                                    <form className="row g-3 needs-validation" noValidate onSubmit={handleLogin}>
+                                        <div className="col-12">
+                                            <label htmlFor="yourUsername" className="form-label">Username</label>
+                                            <div className="input-group has-validation">
+                                                <span className="input-group-text" id="inputGroupPrepend">@</span>
+                                                <input type="text" name="username" className="form-control" id="yourUsername" required />
+                                                <div className="invalid-feedback">Please enter your username.</div>
+                                            </div>
+                                        </div>
+                                        <div className="col-12">
+                                            <label htmlFor="yourPassword" className="form-label">Password</label>
+                                            <input type="password" name="password" className="form-control" id="yourPassword" required />
+                                            <div className="invalid-feedback">Please enter your password!</div>
+                                        </div>
+                                        <div className="col-12">
+                                            <div className="form-check">
+                                                <input className="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe" />
+                                                <label className="form-check-label" htmlFor="rememberMe">Remember me</label>
+                                            </div>
+                                        </div>
+                                        <div className="col-12">
+                                            <button className="btn btn-primary w-100" type="submit">
+                                                Login
+                                            </button>
+                                        </div>
+                                        <div className="col-12">
+                                            <p className="small mb-0">
+                                                Don't have account? <a href="pages-register.html">Create an account</a>
+                                            </p>
+                                        </div>
+                                    </form>
+                                </div>
+                                </div>
+                                <div className="credits"> Loved by <a href="https://freeetemplates.com/">FreeeTemplates</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
-            <h1>Login page</h1>
-            <div className="box">
-                <div>
-                    <input type="text" placeholder="Username" />
-                </div>
-                <div>
-                    <input type="password" placeholder="Password" />
-                </div>
-                <button onClick={(e) => handleLogin(e)}>Log in</button>
-            </div>
-        </div>
+        </main>
     )
 }
 
