8c71d835864401698a54067941a0feeaef59ff62
sanyath007 <sanyath007@gmail.com>
2022-12-11 17:17:35 +0700

    Create new api module as axios object and call axios interceptors.request to set its header with access token

---
+++ resources/js/api/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: process.env.MIX_APP_URL
+})
+
+api.interceptors.request.use((config) => {
+    const token = JSON.parse(localStorage.getItem('access_token'))
+    console.log(token);
+
+    // if (token) {
+    //     config.headers.Authorization = `Bearer ${token}`;
+    // }
+
+    return config
+}, (error) => Promise.reject(error))
+
+export default api
