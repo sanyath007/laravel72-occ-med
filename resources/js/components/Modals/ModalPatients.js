1af93353526dff716a2953b1da6b6ff01217bb1f
sanyath007 <sanyath007@gmail.com>
2022-12-18 21:06:37 +0700

    Modify ModalPatients component by adding react-bootstrap Pagination and filtering inputs and create new pager and setPager state and handlePageBtnClicked method for handling onClick evnet of each of pagination's button. Lastly reflactor getPatients method  by adding path argiment with default value and pass its as api endpoint to get method of axios

--- resources/js/components/Modals/ModalPatients.js
+++ resources/js/components/Modals/ModalPatients.js
@@ -1,8 +1,36 @@
-import React, { useState } from 'react'
-import { Modal } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Modal, Pagination } from 'react-bootstrap'
+import api from '../../api'
 
 const ModalPatients = ({ isOpen, hideModal, ...props }) => {
     const [patients, setPatients] = useState([])
+    const [pager, setPager] = useState(null)
+
+    useEffect(() => {
+        getPatients()
+
+        return () => getPatients
+    }, [])
+
+    const getPatients = async (path='/api/patients') => {
+        /** Add filtering logic */
+        console.log(path);
+        /** Add filtering logic */
+
+        const res = await api.get(path)
+
+        if (res.data) {
+            const { data, ...pager } = res.data.patients
+
+            setPatients(data)
+            setPager(pager)
+        }
+    }
+
+    const handlePageBtnClicked = (path) => {
+        getPatients(path)
+    }
 
     return (
         <Modal
@@ -10,9 +38,20 @@ const ModalPatients = ({ isOpen, hideModal, ...props }) => {
             onHide={hideModal}
             size="xl"
         >
-            <Modal.Header closeButton>รายการผู้ป่วย</Modal.Header>
+            <Modal.Header closeButton>รายการผู้ป่วยทั้งหมด</Modal.Header>
             <Modal.Body>
-                <table className="table table-striped table-bordered">
+                <div className="alert border-dark alert-dismissible fade show" role="alert">
+                    <div className="row">
+                        <div className="col-md-3">
+                            <input type="text" name="" className="form-control" placeholder="ค้นหาด้วย HN" />
+                        </div>
+                        <div className="col-md-9">
+                            <input type="text" name="" className="form-control" placeholder="ค้นหาด้วยชื่อ" />
+                        </div>
+                    </div>
+                </div>
+
+                <table className="table table-striped table-bordered mb-0">
                     <thead>
                         <tr>
                             <th scope="col" style={{ width: '3%', textAlign: 'center' }}>#</th>
@@ -21,30 +60,52 @@ const ModalPatients = ({ isOpen, hideModal, ...props }) => {
                             <th scope="col" style={{ width: '8%', textAlign: 'center' }}>CID</th>
                             <th scope="col" style={{ width: '15%', textAlign: 'center' }}>วันเกิด</th>
                             <th scope="col" style={{ width: '6%', textAlign: 'center' }}>อายุ</th>
-                            <th scope="col" style={{ width: '10%', textAlign: 'center' }}>Actions</th>
+                            <th scope="col" style={{ width: '5%', textAlign: 'center' }}>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
                         {patients && patients.map((patient, row) => (
                             <tr key={patient.hn}>
-                                <th scope="row" style={{ textAlign: 'center' }}>{row+1}</th>
+                                <th scope="row" style={{ textAlign: 'center' }}>{pager?.from+row}</th>
                                 <td style={{ textAlign: 'center' }}>{patient.hn}</td>
                                 <td>{patient.pname+patient.fname+ ' ' +patient.lname}</td>
                                 <td style={{ textAlign: 'center' }}>{patient.cid}</td>
                                 <td style={{ textAlign: 'center' }}>{patient.birthdate}</td>
                                 <td style={{ textAlign: 'center' }}>{patient.sex}</td>
                                 <td style={{ textAlign: 'center' }}>
-                                    <div className="btn-group" role="group" aria-label="Basic mixed styles example">
-                                        <Link to={`/patients/${patient.id}/detail`} className="btn btn-primary btn-sm">
-                                            <i className="bi bi-search"></i>
-                                        </Link>
-                                    </div>
+                                    <Link to={`/patients/${patient.id}/detail`} className="btn btn-primary btn-sm">
+                                        <i className="bi bi-download"></i>
+                                    </Link>
                                 </td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
             </Modal.Body>
+            <Modal.Footer>
+                <div className="d-flex justify-content-between align-items-center my-0 w-100">
+                    <span className="mb-0">รายการทั้งหมด {pager?.total} รายการ</span>
+                    <span className="mb-0">หน้า {pager?.current_page}/{pager?.last_page}</span>
+                    <Pagination className="mb-0">
+                        <Pagination.First
+                            disabled={pager?.current_page === 1}
+                            onClick={() => handlePageBtnClicked(pager?.first_page_url)}
+                        />
+                        <Pagination.Prev
+                            disabled={pager?.current_page === 1}
+                            onClick={() => handlePageBtnClicked(pager?.prev_page_url)}
+                        />
+                        <Pagination.Next
+                            disabled={pager?.current_page === pager?.last_page}
+                            onClick={() => handlePageBtnClicked(pager?.next_page_url)}
+                        />
+                        <Pagination.Last
+                            disabled={pager?.current_page === pager?.last_page}
+                            onClick={() => handlePageBtnClicked(pager?.last_page_url)}
+                        />
+                    </Pagination>
+                </div>
+            </Modal.Footer>
         </Modal>
     )
 }
