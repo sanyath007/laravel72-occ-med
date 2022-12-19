77d0250fa9cb2a42d079db97e132f4bc13edaf49
sanyath007 <sanyath007@gmail.com>
2022-12-19 15:39:02 +0700

    Re-design CompanyForm component by using Formik and Yup components for manipulating form data

--- resources/js/pages/Companies/CompanyForm.js
+++ resources/js/pages/Companies/CompanyForm.js
@@ -1,8 +1,22 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Formik, Form, Field } from 'formik'
+import * as Yup from 'yup'
 import Pagination from '../../components/Pagination'
+import { FaSave } from 'react-icons/fa'
 import { GlobalContext } from '../../context/globalContext'
 import api from '../../api'
+
+const companySchema = Yup.object().shape({
+    name: Yup.string().required(),
+    company_type_id: Yup.string().required(),
+    address: Yup.string().required(),
+    tambon_id: Yup.string().required(),
+    amphur_id: Yup.string().required(),
+    changwat_id: Yup.string().required(),
+    zipcode: Yup.string().required(),
+    tel: Yup.string().required(),
+})
 
 const CompanyForm = () => {
     const { id } = useParams()
@@ -34,6 +48,10 @@ const CompanyForm = () => {
         }
     }
 
+    const handleSubmit = (values) => {
+        console.log(values)
+    }
+
     return (
         <section className="section">
             <div className="row">
@@ -43,7 +61,245 @@ const CompanyForm = () => {
                             <h5 className="card-title">
                                 {id ? 'แก้ไขรายงานสถานประกอบการ' : 'เพิ่มรายงานสถานประกอบการ'}
                             </h5>
-                            
+
+                            <Formik
+                                initialValues={{
+                                    id: '',
+                                    name: '',
+                                    company_type_id: '',
+                                    address: '',
+                                    moo: '',
+                                    road: '',
+                                    tambon_id: '',
+                                    amphur_id: '',
+                                    changwat_id: '',
+                                    zipcode: '',
+                                    tel: '',
+                                    email: '',
+                                    coordinates: '',
+                                    contact_name: '',
+                                    contact_tel: '',
+                                    contact_email: '',
+                                    remark: ''
+                                }}
+                                validationSchema={companySchema}
+                                onSubmit={handleSubmit}
+                            >
+                                {(formProps) => (
+                                    <Form>
+                                        <div className="row mb-3">
+                                            <div className="col-md-6 form-group">
+                                                <label htmlFor="">ชื่อสถานประกอบการ</label>
+                                                <input
+                                                    type="text"
+                                                    name="name"
+                                                    value={formProps.values.name}
+                                                    onChange={formProps.handleChange}
+                                                    className={`form-control ${formProps.errors.name && formProps.touched.name ? 'is-invalid' : ''}`}
+                                                />
+                                                {formProps.errors.name && formProps.touched.name ? (
+                                                    <div className="invalid-feedback">
+                                                        {formProps.errors.name}
+                                                    </div>
+                                                ) : null }
+                                            </div>
+                                            <div className="col-md-6 form-group mb-2">
+                                                <label htmlFor="">ประเภทสถานประกอบการ</label>
+                                                <select
+                                                    name="company_type_id"
+                                                    value={formProps.values.company_type_id}
+                                                    onChange={formProps.handleChange}
+                                                    className={`form-control ${formProps.errors.company_type_id && formProps.touched.company_type_id ? 'is-invalid' : ''}`}
+                                                >
+                                                    <option value=""></option>
+                                                </select>
+                                                {formProps.errors.company_type_id && formProps.touched.company_type_id ? (
+                                                    <div className="invalid-feedback">
+                                                        {formProps.errors.company_type_id}
+                                                    </div>
+                                                ) : null }
+                                            </div>
+                                            <div className="col-md-6 form-group mb-2">
+                                                <label htmlFor="">ที่อยู่ เลขที่</label>
+                                                <input
+                                                    type="text"
+                                                    name="address"
+                                                    value={formProps.values.address}
+                                                    onChange={formProps.handleChange}
+                                                    className={`form-control ${formProps.errors.address && formProps.touched.address ? 'is-invalid' : ''}`}
+                                                />
+                                                {formProps.errors.address && formProps.touched.address ? (
+                                                    <div className="invalid-feedback">
+                                                        {formProps.errors.address}
+                                                    </div>
+                                                ) : null }
+                                            </div>
+                                            <div className="col-md-2 form-group mb-2">
+                                                <label htmlFor="">หมู่ที่</label>
+                                                <input
+                                                    type="text"
+                                                    name="moo"
+                                                    value={formProps.values.moo}
+                                                    onChange={formProps.handleChange}
+                                                    className="form-control"
+                                                />
+                                            </div>
+                                            <div className="col-md-4 form-group mb-2">
+                                                <label htmlFor="">ถนน</label>
+                                                <input
+                                                    type="text"
+                                                    name="road"
+                                                    value={formProps.values.road}
+                                                    onChange={formProps.handleChange}
+                                                    className="form-control"
+                                                />
+                                            </div>
+                                            <div className="col-md-4 form-group mb-2">
+                                                <label htmlFor="">จังหวัด</label>
+                                                <select
+                                                    name="changwat_id"
+                                                    value={formProps.values.changwat_id}
+                                                    onChange={formProps.handleChange}
+                                                    className={`form-control ${formProps.errors.changwat_id && formProps.touched.changwat_id ? 'is-invalid' : ''}`}
+                                                >
+                                                    <option value=""></option>
+                                                </select>
+                                                {formProps.errors.changwat_id && formProps.touched.changwat_id ? (
+                                                    <div className="invalid-feedback">
+                                                        {formProps.errors.changwat_id}
+                                                    </div>
+                                                ) : null }
+                                            </div>
+                                            <div className="col-md-4 form-group mb-2">
+                                                <label htmlFor="">อำเภอ</label>
+                                                <select
+                                                    name="amphur_id"
+                                                    value={formProps.values.amphur_id}
+                                                    onChange={formProps.handleChange}
+                                                    className={`form-control ${formProps.errors.amphur_id && formProps.touched.amphur_id ? 'is-invalid' : ''}`}
+                                                >
+                                                    <option value=""></option>
+                                                </select>
+                                                {formProps.errors.amphur_id && formProps.touched.amphur_id ? (
+                                                    <div className="invalid-feedback">
+                                                        {formProps.errors.amphur_id}
+                                                    </div>
+                                                ) : null }
+                                            </div>
+                                            <div className="col-md-4 form-group mb-2">
+                                                <label htmlFor="">ตำบล</label>
+                                                <select
+                                                    name="tambon_id"
+                                                    value={formProps.values.tambon_id}
+                                                    onChange={formProps.handleChange}
+                                                    className={`form-control ${formProps.errors.tambon_id && formProps.touched.tambon_id ? 'is-invalid' : ''}`}
+                                                >
+                                                    <option value=""></option>
+                                                </select>
+                                                {formProps.errors.tambon_id && formProps.touched.tambon_id ? (
+                                                    <div className="invalid-feedback">
+                                                        {formProps.errors.tambon_id}
+                                                    </div>
+                                                ) : null }
+                                            </div>
+                                            <div className="col-md-2 form-group mb-2">
+                                                <label htmlFor="">รหัสไปรษณีย์</label>
+                                                <input
+                                                    type="text"
+                                                    name="zipcode"
+                                                    value={formProps.values.zipcode}
+                                                    onChange={formProps.handleChange}
+                                                    className={`form-control ${formProps.errors.zipcode && formProps.touched.zipcode ? 'is-invalid' : ''}`}
+                                                />
+                                                {formProps.errors.zipcode && formProps.touched.zipcode ? (
+                                                    <div className="invalid-feedback">
+                                                        {formProps.errors.zipcode}
+                                                    </div>
+                                                ) : null }
+                                            </div>
+                                            <div className="col-md-3 form-group mb-2">
+                                                <label htmlFor="">เบอร์โทรศัพท์</label>
+                                                <input
+                                                    type="text"
+                                                    name="tel"
+                                                    value={formProps.values.tel}
+                                                    onChange={formProps.handleChange}
+                                                    className={`form-control ${formProps.errors.tel && formProps.touched.tel ? 'is-invalid' : ''}`}
+                                                />
+                                                {formProps.errors.tel && formProps.touched.tel ? (
+                                                    <div className="invalid-feedback">
+                                                        {formProps.errors.tel}
+                                                    </div>
+                                                ) : null }
+                                            </div>
+                                            <div className="col-md-4 form-group mb-2">
+                                                <label htmlFor="">E-mail</label>
+                                                <input
+                                                    type="text"
+                                                    name="email"
+                                                    value={formProps.values.email}
+                                                    onChange={formProps.handleChange}
+                                                    className="form-control"
+                                                />
+                                            </div>
+                                            <div className="col-md-3 form-group mb-2">
+                                                <label htmlFor="">พิกัดที่ตั้ง</label>
+                                                <input
+                                                    type="text"
+                                                    name="coordinates"
+                                                    value={formProps.values.coordinates}
+                                                    onChange={formProps.handleChange}
+                                                    className="form-control"
+                                                />
+                                            </div>
+                                            <div className="col-md-4 form-group mb-2">
+                                                <label htmlFor="">ชื่อผู้ติดต่อ</label>
+                                                <input
+                                                    type="text"
+                                                    name="contact_name"
+                                                    value={formProps.values.contact_name}
+                                                    onChange={formProps.handleChange}
+                                                    className="form-control"
+                                                />
+                                            </div>
+                                            <div className="col-md-4 form-group mb-2">
+                                                <label htmlFor="">เบอร์โทรผู้ติดต่อ</label>
+                                                <input
+                                                    type="text"
+                                                    name="contact_tel"
+                                                    value={formProps.values.contact_tel}
+                                                    onChange={formProps.handleChange}
+                                                    className="form-control"
+                                                />
+                                            </div>
+                                            <div className="col-md-4 form-group mb-2">
+                                                <label htmlFor="">E-mail ผู้ติดต่อ</label>
+                                                <input
+                                                    type="text"
+                                                    name="contact_email"
+                                                    value={formProps.values.contact_email}
+                                                    onChange={formProps.handleChange}
+                                                    className="form-control"
+                                                />
+                                            </div>
+                                            <div className="col-md-12 form-group mb-2">
+                                                <label htmlFor="">หมายเหตุ</label>
+                                                <Field
+                                                    as="textarea"
+                                                    name="remark"
+                                                    className="form-control"
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="text-center">
+                                            <button type="submit" className="btn btn-primary">
+                                                <FaSave className="me-1" />
+                                                บันทึก
+                                            </button>
+                                        </div>
+                                    </Form>
+                                )}
+                            </Formik>
                         </div>
                     </div>
                 </div>
