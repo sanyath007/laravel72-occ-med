37fdd7565c08633017c9569403b14591ff726ad1
sanyath007 <sanyath007@gmail.com>
2022-12-20 15:21:09 +0700

    Create new icd10Slice with getIcd10s async thunk action

---
+++ resources/js/store/icd10.js
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import api from '../api'
+
+const initialState = {
+    icd10s: [],
+    icd10: null,
+    pager: null,
+    loading: false
+}
+
+export const getIcd10s = createAsyncThunk('icd10/getIcd10s', async ({ path }) => {
+    try {
+        const res = await api.get(path)
+
+        return res.data.icd10s
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+export const icd10Slice = createSlice({
+    name: 'icd10',
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [getIcd10s.pending]: (state) => {
+            state.loading = true
+        },
+        [getIcd10s.fulfilled]: (state, { payload }) => {
+            const { data, pager } = payload
+
+            state.icd10s = data
+            state.pager = pager
+            state.loading = false
+        },
+        [getIcd10s.rejected]: (state) => {
+            state.loading = false
+        }
+    }
+})
+
+export const icd10Reducer = icd10Slice.reducer
--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { counterReducer } from './counter'
 import { companyReducer } from './company'
+import { icd10Reducer } from './icd10'
 
 export default configureStore({
     reducer: {
         counter: counterReducer,
         company: companyReducer,
+        icd10: icd10Reducer,
     }
 })
