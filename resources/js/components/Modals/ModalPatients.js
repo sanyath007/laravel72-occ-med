158afedc87a06fd54c16944310adc2173cf4f8ff
sanyath007 <sanyath007@gmail.com>
2022-12-23 09:30:03 +0700

    Add and use PatientFilter component instead of old filtering code in ModalPatients

--- resources/js/components/Modals/ModalPatients.js
+++ resources/js/components/Modals/ModalPatients.js
@@ -7,32 +7,23 @@ import api from '../../api'
 import * as moment from 'moment'
 import { getPatients } from '../../store/patient'
 import Pagination from '../Pagination'
+import PatientFilter from '../Patient/PatientFilter'
 
 const ModalPatients = ({ isOpen, hideModal, onSelected, ...props }) => {
     const dispatch = useDispatch()
     const { patients, pager, loading } = useSelector(state => state.patient)
-    const [filterings, setFilterings] = useState({ hn: '', name: '' })
+    const [queryStrings, setQueryStrings] = useState('')
 
     useEffect(() => {
         fetchPatients()
-    }, [filterings])
+    }, [queryStrings])
 
     const fetchPatients = (path='/api/patients?page=') => {
-        /** Add filtering logic */
-        const hn = filterings.hn ? filterings.hn : ''
-        const [fname, lname] = filterings.name ? filterings.name.split(' ') : ' '.split(' ')
-
-        dispatch(getPatients({ path: `${path}&hn=${hn}&name=${fname}-${lname == undefined ? '' : lname}` }))
+        dispatch(getPatients({ path: `${path}${queryStrings}` }))
     }
 
     const handlePageBtnClicked = (path) => {
         fetchPatients(path)
-    }
-
-    const handleChange = (e) => {
-        const { name, value } = e.target
-
-        setFilterings(prevState => ({ ...prevState, [name]: value }))
     }
 
     return (
@@ -44,28 +35,7 @@ const ModalPatients = ({ isOpen, hideModal, onSelected, ...props }) => {
             <Modal.Header closeButton>รายการผู้ป่วยทั้งหมด</Modal.Header>
             <Modal.Body>
                 <div className="alert border-dark alert-dismissible fade show" role="alert">
-                    <div className="row">
-                        <div className="col-md-3">
-                            <input
-                                type="text"
-                                name="hn"
-                                value={filterings.hn}
-                                onChange={handleChange}
-                                className="form-control"
-                                placeholder="ค้นหาด้วย HN"
-                            />
-                        </div>
-                        <div className="col-md-9">
-                            <input
-                                type="text"
-                                name="name"
-                                value={filterings.name}
-                                onChange={handleChange}
-                                className="form-control"
-                                placeholder="ค้นหาด้วยชื่อ สกุล"
-                            />
-                        </div>
-                    </div>
+                    <PatientFilter setQueryStrings={setQueryStrings} />
                 </div>
 
                 <table className="table table-striped table-bordered mb-0">
