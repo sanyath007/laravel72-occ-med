61c870c887d18c546fa536f6a325a2378a791b6d
sanyath007 <sanyath007@gmail.com>
2023-04-06 17:29:07 +0700

    Modify getReportBullets action of store/reportBulletSlice and create new store action to its

--- resources/js/store/reportBullet.js
+++ resources/js/store/reportBullet.js
@@ -8,13 +8,31 @@ const initialState = {
     error: null
 };
 
-export const getReportBullets = createAsyncThunk('reportBullet/getReportBullets', async () => {
+export const getReportBullets = createAsyncThunk('reportBullet/getReportBullets', async ({ path }, { rejectWithValue }) => {
+    try {
+        const res = await api.get(path);
 
+        return res.data;
+    } catch (error) {
+        console.log(error);
+        rejectWithValue(error)
+    }
 });
 
 export const getReportBulletsByDivision = createAsyncThunk('reportBullet/getReportBulletsByDivision', async ({ path }, { rejectWithValue }) => {
     try {
         const res = await api.get(path);
+
+        return res.data;
+    } catch (error) {
+        console.log(error);
+        rejectWithValue(error)
+    }
+});
+
+export const store = createAsyncThunk('reportBullet/store', async ({ data }, { rejectWithValue }) => {
+    try {
+        const res = await api.post('/api/report-bullets', data);
 
         return res.data;
     } catch (error) {
@@ -51,6 +69,19 @@ export const reportBulletSlice = createSlice({
             state.loading = false
         },
         [getReportBulletsByDivision.rejected]: (state, { payload }) => {
+            state.error = payload;
+            state.loading = false
+        },
+        [store.pending]: (state) => {
+            state.loading = true;
+        },
+        [store.fulfilled]: (state, { payload }) => {
+            const newBullet = [ ...state.bullets, payload];
+
+            state.bullets = newBullet;
+            state.loading = false;
+        },
+        [store.rejected]: (state, { payload }) => {
             state.error = payload;
             state.loading = false
         },
