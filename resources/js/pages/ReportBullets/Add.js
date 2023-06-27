3004423f0c6a8e731360c70b9c2c10777100a55b
sanyath007 <sanyath007@gmail.com>
2023-06-27 13:32:56 +0700

    Rename of components of Add and Edit

--- resources/js/pages/ReportBullets/Add.js
+++ resources/js/pages/ReportBullets/Add.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import { GlobalContext } from '../../context/globalContext'
 
-const ReportBulletForm = () => {
+const AddReportBullet = () => {
     const { setGlobal } = useContext(GlobalContext);
 
     useEffect(() => {
@@ -33,4 +33,4 @@ const ReportBulletForm = () => {
     )
 }
 
-export default ReportBulletForm
+export default AddReportBullet
