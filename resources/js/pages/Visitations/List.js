7f4a6eea1f0429644cd862ca6764ebde69c54369
sanyath007 <sanyath007@gmail.com>
2023-10-17 07:22:37 +0700

    Create dispatch const as useDispatch hook and constraints of visitation global states by using useSelector hook and use useEffect hook for calling dispatch getVisitations action of visitationSlice and render visitation data in table data. And create handlePageClick method and render Pagination and pass handlePageClick as onPageClick prop of its in VisitationList

--- resources/js/pages/Visitations/List.js
+++ resources/js/pages/Visitations/List.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { getVisitations } from '../../store/slices/visitation'
+import Loading from '../../components/Loading'
+import Pagination from '../../components/Pagination'
 
 const VisitationList = () => {
+    const dispatch = useDispatch();
+    const { visitations, pager, loading } = useSelector(state => state.visitation);
+
+    useEffect(() => {
+        dispatch(getVisitations({ url: '/api/visitations/search' }));
+    }, []);
+
+    const handlePageClick = (url) => {
+        console.log(url);
+    };
+
     return (
         <section className="section">
             <div className="row">
@@ -20,22 +35,56 @@ const VisitationList = () => {
                                 <h5 className="card-title p-0">รายการติดตามเยี่ยมบ้าน</h5>
 
                                 <div>
-                                    <Link to="/visit-homes/new" className="btn btn-primary">เพิ่มรายการ</Link>
+                                    <Link to="/visitations/new" className="btn btn-primary">เพิ่มรายการ</Link>
                                 </div>
                             </div>
 
                             <div>
-                                <table className="table table-bordered mb-0">
+                                <table className="table table-bordered mb-2">
                                     <thead>
                                         <tr>
                                             <th style={{ width: '5%', textAlign: 'center' }}>#</th>
-                                            <th>ชื่อเอกสาร</th>
-                                            <th style={{ width: '10%', textAlign: 'center' }}>วันที่ Upload</th>
-                                            <th style={{ width: '15%', textAlign: 'center' }}>ผู้ดำเนินการ</th>
+                                            <th style={{ width: '10%', textAlign: 'center' }}>วันที่</th>
+                                            <th>สถานประกอบการ/สถานที่</th>
+                                            <th style={{ width: '20%', textAlign: 'center' }}>ผู้ดำเนินการ</th>
                                             <th style={{ width: '10%', textAlign: 'center' }}>Actions</th>
                                         </tr>
                                     </thead>
+                                    <tbody>
+                                        {loading && (
+                                            <tr>
+                                                <td colSpan={6} className="text-center"><Loading /></td>
+                                            </tr>
+                                        )}
+
+                                        {visitations && visitations.map((visit, index) => (
+                                            <tr key={visit.id}>
+                                                <td style={{ textAlign: 'center' }}>{pager && pager.from+index}</td>
+                                                <td style={{ textAlign: 'center' }}>{visit.visit_date}</td>
+                                                <td></td>
+                                                <td>{visit.division?.name}</td>
+                                                <td style={{ textAlign: 'center' }}>
+                                                    <div className="btn-group" role="group" aria-label="Basic mixed styles example">
+                                                        <Link to={`/visitations/${visit.id}/detail`} className="btn btn-primary btn-sm">
+                                                            <i className="bi bi-search"></i>
+                                                        </Link>
+                                                        <Link to={`/visitations/${visit.id}/edit`} className="btn btn-warning btn-sm">
+                                                            <i className="bi bi-pencil-square"></i>
+                                                        </Link>
+                                                        <a href="#" className="btn btn-danger btn-sm" onClick={(e) => {}}>
+                                                            <i className="bi bi-trash"></i>
+                                                        </a>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
                                 </table>
+
+                                <Pagination
+                                    pager={pager}
+                                    onPageClick={handlePageClick}
+                                />
                             </div>
                         </div>
                     </div>
