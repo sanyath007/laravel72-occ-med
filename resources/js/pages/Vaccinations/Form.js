82b096f9d67c7f317986b2912b43a3da46a5281e
sanyath007 <sanyath007@gmail.com>
2023-10-28 06:49:15 +0700

    Install packages that related Material UI and use them in VaccinationForm  for date selection input and create new utils/OverwriteMomentBE extended from AdapterMoment

--- resources/js/pages/Vaccinations/Form.js
+++ resources/js/pages/Vaccinations/Form.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Formik, Form, Field } from 'formik'
 import * as Yup from 'yup'
 import { Col, Row } from 'react-bootstrap'
 import { FaSave, FaSearch } from 'react-icons/fa'
+import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers'
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+import moment from 'moment'
+import "moment/locale/th";
 
 const vaccinationSchema = Yup.object().shape({});
 
 const VaccinationForm = () => {
+    const [selectedVaccineDate, setSelectedVaccineDate] = useState(moment());
 
     const handleSubmit = (values, formik) => {
 
@@ -15,7 +20,12 @@ const VaccinationForm = () => {
     return (
         <Formik
             initialValues={{
-
+                vaccine_date: '',
+                place_id: '',
+                vaccine_type_id: '',
+                target_group_id: '',
+                num_of_vaccinated: '',
+                num_of_side_effected: '',
             }}
             validationSchema={vaccinationSchema}
             onSubmit={handleSubmit}
@@ -26,7 +36,17 @@ const VaccinationForm = () => {
                         <Row className="mb-2">
                             <Col md={3}>
                                 <label htmlFor="">วันที่เดินสำรวจ</label>
-                                <input type="date" className="form-control" />
+                                <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="th">
+                                    <DatePicker
+                                        format="DD/MM/YYYY"
+                                        value={selectedVaccineDate}
+                                        onChange={(date) => {
+                                            console.log(date);
+                                            setSelectedVaccineDate(date);
+                                            formik.setFieldValue('vaccine_date', date.format('YYYY-MM-DD'));
+                                        }}
+                                    />
+                                </LocalizationProvider>
                             </Col>
                             <Col>
                                 <label htmlFor="">สถานที่ให้วัคซีน</label>
