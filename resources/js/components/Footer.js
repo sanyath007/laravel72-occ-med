2d0ff60fcf60cb4f3d01313b2807eb2d86ac129d
sanyath007 <sanyath007@gmail.com>
2025-03-21 15:49:05 +0700

    reflactor(Footer): edit footer text

--- resources/js/components/Footer.js
+++ resources/js/components/Footer.js
@@ -4,8 +4,8 @@ import { FaFacebook, FaTwitter, FaYoutube } from 'react-icons/fa'
 const Footer = () => {
     return (
         <footer id="footer" className="footer">
-            <div className="copyright"> &copy; Copyright <strong><span>Compnay Name</span></strong>. All Rights Reserved</div>
-            <div className="credits"> with love <a href="https://freeetemplates.com/">FreeeTemplates</a></div>
+            <div className="copyright"> &copy; Copyright <strong><span>กลุ่มงานอาชีวเวชกรรม</span></strong>. All Rights Reserved</div>
+            <div className="credits"><a href="https://mnrh.go.th/" target="_blank">โรงพยาบาลมหาราชนครราชสีมา</a></div>
         </footer>
     )
 }
